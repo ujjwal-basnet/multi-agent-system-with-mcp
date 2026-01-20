790b73d744df38298ce0d3d3f9df3a65b08aaf58
ujjwal-basnet <ujjwalvictus15@gmail.com>
2026-01-20 19:26:14 +0545

    cohere:  update settings to config

--- llm_clients.py
+++ llm_clients.py
@@ -1,4 +1,4 @@
-from settings import settings
+from settings import config
 from openai import OpenAI 
 from langchain_openai import ChatOpenAI
 
@@ -7,12 +7,12 @@ from langchain_openai import ChatOpenAI
 mimo_v2_flash_client = ChatOpenAI(
     model="xiaomi/mimo-v2-flash:free",
     base_url="https://openrouter.ai/api/v1",
-    api_key=settings.OPENROUTER_API_KEY,
+    api_key=config .OPENROUTER_API_KEY,
 )
 
 
 openai_client= OpenAI(
-  api_key= settings.OPENAI_API_KEY,
+  api_key= config.OPENAI_API_KEY,
 )
 
 # response= openai_client.chat.completions.create(
@@ -31,6 +31,20 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Google LLM client
 google_langchain_client = ChatGoogleGenerativeAI(
-    model=settings.GEMINI_MODEL,
-    google_api_key=settings.GEMINAI_API_KEY,
+    model=config.GEMINI_MODEL,
+    google_api_key=config.GEMINAI_API_KEY2,
 )
+
+
+
+from pinecone import Pinecone
+# Initialize a Pinecone client with your API key
+pine_cone_client = Pinecone(api_key=config.PINECONE_API_KEY)
+
+
+from langchain_cohere import ChatCohere,CohereEmbeddings
+cohere_chat_client = ChatCohere( cohere_api_key=config.COHERE_API)
+
+
+from langchain_cohere import CohereEmbeddings
+cohere_embedding_client = CohereEmbeddings(model="embed-english-light-v3.0" , cohere_api_key= config.COHERE_API)
